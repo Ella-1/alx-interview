f72b208d4991557cddce638b427cc77b6d80adff
Ella-1 <es9897288@gmail.com>
2023-01-26 04:56:09 -0800

    task 1

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    result = []
+    if n > 0:
+        for i in range(1, n + 1):
+            average = []
+            k = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                k = k * (i - j) // j
+            result.append(average)
+    return 
